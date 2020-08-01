022c934bc4ab8cf856c84af7f4688798dfe7fb3d
krispex <krispex@gmail.com>
2020-08-01 02:55:23 -0700

    Update main.lua
    removed unneeded code

--- main.lua
+++ main.lua
@@ -17,7 +17,6 @@ local function MyAddonCommandTest(msg, editbox)
     print("[".."|cffff0000"..playerName.."|r".."]".." says: "..msg)
 
     local msg = msg:gsub("(%a)","%1 ")
-    local chatmsg = msg:gsub("%a", {["a"] = "Ÿ",["b"] = "€",["c"] = "À",["d"] = "€",["e"] = "2",["f"] = "ä",["g"] = "À",["h"] = "„",["i"] = "3",["j"] = "3",["k"] = "À",["l"] = "ž",["m"] = "Ä",["n"] = "Š",["o"] = "1",["p"] = "€",["q"] = "À",["r"] = "‡",["s"] = "ƒ",["t"] = "ÇÍ",["u"] = "p",["v"] = "ä",["w"] = "◒",["x"] = "À ƒ",["y"] = "3",["z"] = "ƒ"})
     local edited = msg:gsub("%a", {["c"] = "k", ["d"] = "b", ["f"] = "v", ["g"] = "k",["i"] = "y", ["j"] = "y", ["p"] = "b", ["q"] = "k", ["t"] = "th", ["x"] = "k s", ["z"] = "s"})
     
     print("[".."|cffff0000"..playerName.."|r".."]".." says: "..edited)
